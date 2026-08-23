505fc7d2c4d87d5b34589cea6c8f0385c46987d9
UnorganizedToolbox <unorganizedtoolbox@gmail.com>
2026-08-24 00:50:37 +0900

    chore: add plugins array to vite.config.ts to allow cloudflare integration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './', // Ensures relative asset paths, making it compatible with GitHub Pages sub-directories
+  plugins: [],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
