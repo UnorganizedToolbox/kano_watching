6829cf343316b266273d0ebb25d52bc15fa6c150
UnorganizedToolbox <unorganizedtoolbox@gmail.com>
2026-09-04 17:47:42 +0900

    chore: bump version to v0.0.4.0 for Google Auth feature

--- bump_version.js
+++ bump_version.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 
-// Bump Sidebar
 let sidebar = fs.readFileSync('src/app/(app)/components/Sidebar.tsx', 'utf8');
-sidebar = sidebar.replace(/v0\.0\.2\.0/g, 'v0.0.3.0');
+sidebar = sidebar.replace(/v0\.0\.3\.1/g, 'v0.0.4.0');
 fs.writeFileSync('src/app/(app)/components/Sidebar.tsx', sidebar);
 
-// Bump Project Memory
 let memory = fs.readFileSync('PROJECT_MEMORY.md', 'utf8');
-memory = memory.replace(/v0\.0\.2\.0/g, 'v0.0.3.0');
+memory = memory.replace(/v0\.0\.3\.1/g, 'v0.0.4.0');
 fs.writeFileSync('PROJECT_MEMORY.md', memory);
